c9f168c2a369e7329dc9daf36aba790ee87f7797
Ashley Lee <ashleyhayjinlee@gmail.com>
2021-02-04 22:41:27 -0600

    Updates

--- script.js
+++ script.js
@@ -1,27 +1,26 @@
 var apiKey = "c3de2d2c940e3b4a114f178684ecc6fd";
-// var city = localStorage.getItem("latestcity");
-var city = "Austin";
+var city = localStorage.getItem("savedCity");
+// var city = "Austin";
 // query URL for one day forcast in a city
-var queryUrl =  "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+var queryUrl =
+  "http://api.openweathermap.org/data/2.5/weather?q=" +
+  city +
+  "&appid=" +
+  apiKey;
 
-async function getWeather (){
-    const resp = await fetch(
-        queryUrl,
-        {
-            method: "GET",
-        }
+// function to get weather for a city 
+async function getWeather() {
+  const resp = await fetch(queryUrl, {
+    method: "GET",
+  });
 
-    );
-    
-    const data = await resp.json();
+  const data = await resp.json();
 
-    console.log(data);
+  console.log(data);
 }
 
-getWeather();
-
-
-
 // function saveCity (){
-//     var queryUrl =  
+//     var queryUrl =
 // }
+
+$("#9-am .description").val(localStorage.getItem("9am"));
