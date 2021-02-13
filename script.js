61cec7a1fa52e2cf1f37a51ef0a77d1afc42516b
Ashley Lee <ashleyhayjinlee@gmail.com>
2021-02-13 00:36:16 -0600

    Woogit add .

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var apiKey = "c3de2d2c940e3b4a114f178684ecc6fd";
 var city = localStorage.getItem("savedCity");
 var savedCity = [];
-var cityList;
+// var cityList;
 // var city = "Austin";
 // query URL for one day forcast in a city
 var queryUrl =
@@ -10,21 +10,34 @@ var queryUrl =
   "&appid=" +
   apiKey;
 
-$(".submit-btn").click(function(){
-  
+$(".submit-btn").click(function () {
+  var dataCity = {
+    savedCityItem: $(this).prev(".city-input").val(),
+  };
 
-  cityList = $(".city-input").val();
-  savedCity.push(cityList); 
+  var saved = JSON.parse(localStorage.getItem("savedCity")) || [];
+  saved.push(dataCity);
+  localStorage.setItem("savedCity", JSON.stringify(saved));
+  console.log(saved.length);
+  var cityList = JSON.parse(localStorage.getItem("savedCity"));
 
-  console.log(savedCity);
+  function cityListPage() {
+    for (i = 0; i < cityList.length; i++) {
+      var myDiv = $(".cityList");
+      var paragraph = document.createElement("p");
+      paragraph.setAttribute("class", "p" + [i]);
+      paragraph.textContent = cityList[i].savedCityItem;
 
-  localStorage.setItem('savedCity', JSON.stringify(savedCity));
+      // if (obj.length > 0){
+    }
+    myDiv.append(paragraph);
 
-  $("#cityList").append()
+  }
 
-  });
+  cityListPage();
+});
 
-// function to get weather for a specific city 
+// function to get weather for a specific city
 async function getWeather() {
   const resp = await fetch(queryUrl, {
     method: "GET",
@@ -33,12 +46,8 @@ async function getWeather() {
   const data = await resp.json();
 
   console.log(data);
-  
 
   var cityWeather = data.main.temp;
-};
-
+}
 
 getWeather();
-
-
